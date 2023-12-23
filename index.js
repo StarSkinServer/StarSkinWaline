bd5752e724089104f56caaff77876c1d59968bb8
ShanRenYi - CTB <88585893+SRY-CTB@users.noreply.github.com>
2023-12-23 13:02:40 +0800

    Update 真实IP

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const Application = require('@waline/vercel');
+const Waline = require('@waline/vercel');
+const HelloWorldPlugin = require('@waline-plugins/hello-world');
 
-module.exports = Application({
-  plugins: [],
+module.exports = Waline({
+  plugins: [HelloWorldPlugin],
   async postSave(comment) {
-    // do what ever you want after comment saved
+    // 真实IP
+  },
+  think: {
+    app: {
+      proxyIpHeader: "CF-Connecting-IP",
+    },
   },
 });
